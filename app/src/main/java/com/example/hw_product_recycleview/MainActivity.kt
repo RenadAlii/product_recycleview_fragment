79175ab6cc9ecaafa74d6aafd47b484dd7a6a181
Renad <Renad.Ali.ruh.k@tuwaiq.edu.sa>
2021-11-07 02:34:52 +0300

    sol

--- app/src/main/java/com/example/hw_product_recycleview/MainActivity.kt
+++ app/src/main/java/com/example/hw_product_recycleview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.hw_product_recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hw_product_recycleview.adapter.ProductListAdapter
+import com.example.hw_product_recycleview.databinding.ActivityMainBinding
+import com.example.hw_product_recycleview.data.DataSource
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myDataset = DataSource.dataSet
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ProductListAdapter(this, myDataset)
+
+
     }
 }
